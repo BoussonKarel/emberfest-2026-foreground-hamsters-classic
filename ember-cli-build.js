0af46c9c7ce96133a128a0156212f2f3b7cab043
Karel Bousson <bousson.karel@gmail.com>
2026-09-14 22:05:19 +0200

    build: first use of  broccoli plugins (broccoli-string-replace + broccoli-funnel)

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,6 +1,13 @@
 'use strict';
 
+const funnel = require('broccoli-funnel');
+const replace = require('broccoli-string-replace');
+
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+const isDevelopment = EmberApp.env() === 'development';
+
+const packageJson = require('./package.json');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
@@ -27,5 +34,27 @@ module.exports = function (defaults) {
         devtool: 'cheap-source-map',
       },
     },
+    extraPublicTrees: [
+      // Support different values per environment:
+      replace(funnel('chrome-extension', { files: ['manifest.json'] }), {
+        patterns: [
+          {
+            match: /{{EXT_NAME}}/g,
+            replacement: 'Classic Hamster Wacking',
+          },
+          {
+            match: /{{EXT_VERSION}}/g,
+            replacement: packageJson.version,
+          },
+          {
+            match: /{{EXT_KEY}}/g,
+            replacement: isDevelopment
+              ? 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsbzLw78qwq6ZfdN+aRsCUfq180/EAwhs3XXh7HxqVSIF0ZvB9sg/dNNjbTm5gopGgsWPgQ+KwDiZzyXNeSNz6HOrgNGKOGSCq7TNffCFE9HG4F/v4ckXxcsLbHz1IOvrPqgCM5aCEHuUM+L/wynL9a2CgUeA9gf7SL/sKrMXlfhd2BASZ7rnS/d838dQ+iW3MjBI5SM5BXYx+VdN0T1j49I0zRI8jZ9VZEEnJRcDKtULZZBb4ShvlRtZ5BkllAnw/VtxEBcw+zsJrr9q+Sf2TqS/rFOXp2FbxKPBxHzvSd/uo5HYSnlAmMjo+98CiN05CcPOHcJzy0v6XdmfnDwcowIDAQAB'
+              : 'GENERATED_BY_CHROME',
+          },
+        ],
+      }),
+      funnel('chrome-extension', { files: ['background.js', 'hamsters.js'] }),
+    ],
   });
 };
